2613afc0a8cad5529fb981879e5ebae334508100
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2023-05-06 16:52:36 -0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let list = document.querySelectorAll('.navegation li ');
+function activeLink(){
+    list.forEach((item) => 
+    item.classList.remove('hovered'));
+    this.classList.add('hovered')
+}
+list.forEach((item) =>
+item.addEventListener('mouseover', activeLink)
+);
