f560805bc54aae9e3c7164e5880cf429fcb17564
Carlos-mg-19 <115847662+Carlos-mg-19@users.noreply.github.com>
2023-05-23 21:50:49 -0300

    commit

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function activeLink(){
 list.forEach((item) =>
 item.addEventListener('click', activeLink)
 );
+
+let navegation = document.querySelector('.navegation')
+let toggle = document.querySelector('.toggle')
+let main = document.querySelector('.main')
+
+toggle.onclick = function(){
+    navegation.classList.toggle('active')
+    main.classList.toggle('active')
+}
